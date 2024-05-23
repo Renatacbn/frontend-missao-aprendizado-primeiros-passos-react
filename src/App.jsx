e500165ca37444368ddde14a2de1defdfe06899d
Renata Nunes <134668464+Renatacbn@users.noreply.github.com>
2024-05-23 16:42:59 +0000

    Criação do HTML e CSS para o Card

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,21 @@ import './App.css'
 function App() {
   return (
     <>
-      App!!
+      <div className='card'>
+        <h1>Java</h1>
+        <img src="https://www.salvatore.academy/devmon/1_java.png" width="200" alt="Imagem de um personagem imitando um alienígena na cor laranja com pintas azul claro, a cabeça é um olho grande e no corpo apenas duas pernas sem braços. " />
+      </div>
+
+      <div className='card'>
+        <h1>Kotlin</h1>
+        <img src="https://www.salvatore.academy/devmon/2_kotlin.png" width="200" alt="Imagem de um personagem imitando um alienígena na cor laranja com pintas azul claro, a cabeça dois olhos grandes, com um pescoço cada.Braços e pernar imitando dinossauros. " />
+      </div>
+
+      <div className='card'>
+        <h1>Android</h1>
+        <img src="https://www.salvatore.academy/devmon/3_android.png" width="200" alt="Imagem de um personagem imitando um alienígena na cor laranja com pintas amarelas, a cabeça três olhos grandes em formato de triângulo, o central tem um pescoçinho.Braços e pernar imitando dinossauros. " />
+      </div>
+      
     </>
   )
 }
